db5bf3b4fe9bf08801a357d1e6bd3158f6c2d041
pavan kvch <pavankvch@gmail.com>
2020-06-24 09:34:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/androindian/firstflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.androindian.firstflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(
+    Center(child: Text(
+      "Hi Flutter",
+      textDirection: TextDirection.ltr)
+    ),
+  );
+}
