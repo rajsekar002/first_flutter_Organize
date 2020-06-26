c720d56af23812216b8ea0d48ae446e5d58df8cb
pavan kvch <pavankvch@gmail.com>
2020-06-26 10:52:04 +0530

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main(){
-
   runApp(
-    Center(child: Text(
-      "Hi Flutter",
-      textDirection: TextDirection.ltr)
+  MaterialApp(
+    title: "My First App",
+    home: Scaffold(
+      appBar: AppBar(title: Text("My Flutter"),),
+      body: Material(
+        color: Colors.blue,
+        child:  Center(child: Text(
+          "Hi Flutter",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 40.0),
+        )
+        ),
+      ),
     ),
+    )
+
+
+
   );
+
+
+
 }
